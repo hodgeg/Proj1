043b9c82d425c26f5536ab443d114d2184aa38f9
george  hodge <hodge.george@rocketmail.com>
2019-04-10 08:52:32 +0000

    had a crack still yet to go rotation cypher

--- project.c
+++ project.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
-int main () {
-printf("yeet")
+
+    void rotation cypher(char* plain text, int key);
+
+int main (void) {
+
+    int key=1
+    char plain text[101]
+    
 return 0;
  }
+
+void rotation cypher(char* plain text, int key);
