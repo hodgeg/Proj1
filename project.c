6605ed10cce765a351a044e7afdc04817bc832e8
George hodge <hodge.george@rocketmail.com>
2019-04-27 05:08:10 +0000

    getting there

--- project.c
+++ project.c
@@ -1,15 +1,43 @@
+//rotation cipher encoding practice
+
+
 #include <stdio.h>
 #include <string.h>
 
 
-    void rotation cypher(char* plain text, int key);
+void rotation cipher(char* message, int key);
 
 int main (void) {
 
-    int key=1
-    char plain text[101]
+    int key;
+    char message[200]
     
-return 0;
+    printf("enter the message to be encrypted");
+    
+        scanf(%s, message)
+    
+    printf("please enter key");
+    
+        scanf(%d, &key)
+    
+    printf("your encrypted code is:" %s);
+    
+
  }
 
-void rotation cypher(char* plain text, int key);
+void rotation cipher(char* message, int key){     
+   
+   int i=0;
+   int cipher value;
+    char cipher;
+
+while( message[i]!= '0' && strlen(message)-1 >i, i++){
+   cipher value= ((int)message[i]-97 + key) %26 +97
+   cipher= (char)(cipher value)
+    
+
+printf("%s, cipher")
+}
+
+}
+
