b118e4c15dda496da9c9aec43233814a20d8829b
george  hodge <hodge.george@rocketmail.com>
2019-04-29 05:11:59 +0000

    commenting for encrypting done

--- Rotation_decryption/main.c
+++ Rotation_decryption/main.c
@@ -1,7 +1,8 @@
 /*The rotation(caesar) cipher is an encoding tool used to encode a message by rotating 
     the letters of the message to appear as random letters
-- A key is also required to 
-        for example; ATTACK AT SUNRISE
+- A key is also required to be hardcoded in where applicable
+        the key will determine the amount of letters to the right the code will rotate the charachters
+        
         
 
 */
@@ -10,11 +11,11 @@
 
 
 
-void rotation_cipher(char* message, int key);
+void rotation_cipher(char* message, int key); // the function definition is declared at the start to be called later
 
-int main (void) {
+int main (void) { // main function is of type void as we dont require it to recycle anything
     FILE*msg;
-    msg=fopen("input","r");
+    msg=fopen("input","r"); // creating files as a solution to allow whitespace in the code otherwise it would not include a space charachter 
     if(msg==0)
     {
     perror("ERROR while opening the file.\n");
@@ -25,13 +26,15 @@ int main (void) {
     while((ch=fgetc(msg)) !=EOF)
 {
     message[i]=ch;
-    i++;
+    i++; // increment the integer of i to ensure there is no infinite loop
 }
 
     
-    int key=1;
+    int key=1; 
+    //key needs to be input by the user
     
-    printf("Please Enter the message to be decrypted\n");
+    printf("Please Enter the message to be encrypted\n"); 
+    // creating a menu like look to create a neat input page
     
     
     
@@ -49,24 +52,26 @@ return 0;
  
 
 void rotation_cipher(char *message, int key)
+// function prototype is called here with the data type and calling the 'message' and 'key' from the int main 
 {     
    
-int i=0;
+int i=0; // declaring the int variable as 0 to start the cipher function with the first charachter of message input
 while( message[i]!= 0)
+// while loop to ensure that the code only accepts a message that is not a NULL charachter
 {
     
-    if (message[i]>64 && message[i]<91)
+    if (message[i]>64 && message[i]<91)// ensuring that the code is an UPPER CASE charachter as the ascii code goes up to 127 
     {
-        message[i]=message[i]-key;
+        message[i]=message[i]-key;// the main 
     
         
-        if(message[i]<65)
+        if(message[i]<65) // ensures the code does not exceed the ASCII code of the upper case letters to ensure a symbol or lower case letter gets pulled into the rotation 
         {
-            message[i]=message[i]-26;
+            message[i]=message[i]-26; 
         }
     }  
-i++;
+i++;// incrementing i to ensure there is no infinite loop 
 }
-puts(message);
+puts(message); // essentially the print function which sends the now rotated message back to the main print function 
 }
 
